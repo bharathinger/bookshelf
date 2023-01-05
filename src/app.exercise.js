27f95822d10825b0ec532b89945a0521ba93019e
bharathinger <bharat3987@outlook.com>
2023-01-05 16:50:34 +0530

    Handle login on page reload

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -5,6 +5,17 @@ import * as React from 'react'
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from './utils/api-client'
+
+const getUser = async () => {
+  let user = null
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+  return user
+}
 
 function App() {
   const [user, setUser] = React.useState(null)
@@ -15,6 +26,10 @@ function App() {
     auth.logout()
     setUser(null)
   }
+
+  React.useEffect(() => {
+    getUser().then(userData => setUser(userData))
+  }, [])
 
   return user ? (
     <AuthenticatedApp user={user} logout={logout} />
