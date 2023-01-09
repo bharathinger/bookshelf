f61ecb68203b7f858de09f8d64e684ebb27e097a
bharathinger <bharat3987@outlook.com>
2023-01-09 16:57:41 +0530

    Update a book rating with useMutation

--- src/components/rating.exercise.js
+++ src/components/rating.exercise.js
@@ -4,6 +4,8 @@ import {jsx} from '@emotion/core'
 import * as React from 'react'
 // 🐨 you'll need useMutation and queryCache from react-query
 // 🐨 you'll also need the client from utils/api-client
+import {useMutation, queryCache} from 'react-query'
+import {client} from 'utils/api-client'
 import {FaStar} from 'react-icons/fa'
 import * as colors from 'styles/colors'
 
@@ -26,7 +28,15 @@ function Rating({listItem, user}) {
   //   you can pass as data.
   // 💰 if you want to get the list-items cache updated after this query finishes
   // then use the `onSettled` config option to queryCache.invalidateQueries('list-items')
-  const update = () => {}
+  const [update] = useMutation(
+    updates =>
+      client(`list-items/${updates.id}`, {
+        method: 'PUT',
+        token: user.token,
+        data: updates,
+      }),
+    {onSettled: () => queryCache.invalidateQueries('list-items')},
+  )
 
   React.useEffect(() => {
     function handleKeyDown(event) {
