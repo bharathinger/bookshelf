7f6d0b4bf5b0744a94755500d8658468d18a4590
bharathinger <bharat3987@outlook.com>
2023-01-10 19:26:36 +0530

    Add error boundary for react queries

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -3,12 +3,32 @@ import './bootstrap'
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
 import {App} from './app'
+import {ReactQueryConfigProvider} from 'react-query'
 
 // ignore the rootRef in this file. I'm just doing it here to make
 // the tests I write to check your work easier.
+const queryConfig = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry(failureCount, error) {
+      if (error.status === 404) {
+        return false
+      } else if (failureCount < 2) {
+        return true
+      } else {
+        return false
+      }
+    },
+  },
+}
 export const rootRef = {}
 loadDevTools(() => {
   const root = createRoot(document.getElementById('root'))
-  root.render(<App />)
+  root.render(
+    <ReactQueryConfigProvider config={queryConfig}>
+      <App />
+    </ReactQueryConfigProvider>,
+  )
   rootRef.current = root
 })
