6d8ccc7d0e4989f601d17e32c29a9b70166c8dda
bharathinger <bharat3987@outlook.com>
2023-01-18 17:12:25 +0530

    Setup a server to test requests

--- src/utils/__tests__/api-client.exercise.js
+++ src/utils/__tests__/api-client.exercise.js
@@ -1,17 +1,26 @@
-// 🐨 you'll need the test server
-// 💰 the way that our tests are set up, you'll find this in `src/test/server/test-server.js`
-// import {server, rest} from 'test/server'
-// 🐨 grab the client
-// import {client} from '../api-client'
+import { server, rest } from 'test/server'
+import { client } from '../api-client'
 
 // 🐨 add a beforeAll to start the server with `server.listen()`
+beforeAll(() => server.listen())
 // 🐨 add an afterAll to stop the server when `server.close()`
+afterAll(() => server.close())
 // 🐨 afterEach test, reset the server handlers to their original handlers
 // via `server.resetHandlers()`
-
+afterEach(() => server.restoreHandlers())
+const apiURL = process.env.REACT_APP_API_URL
 // 🐨 flesh these out:
 
-test.todo('calls fetch at the endpoint with the arguments for GET requests')
+test('calls fetch at the endpoint with the arguments for GET requests', async () => {
+  const endpoint = 'test-endpoint'
+  const mockResult = { mockValue: 'VALUE' }
+  server.use(rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+    return res(ctx.json(mockResult))
+  }))
+  const result = await client(endpoint)
+  console.log('result: ', result)
+  expect(result).toEqual(mockResult)
+})
 // 🐨 add a server handler to handle a test request you'll be making
 // 💰 because this is the first one, I'll give you the code for how to do that.
 // const endpoint = 'test-endpoint'
@@ -25,7 +34,7 @@ test.todo('calls fetch at the endpoint with the arguments for GET requests')
 // 🐨 call the client (don't forget that it's asynchronous)
 // 🐨 assert that the resolved value from the client call is correct
 
-test.todo('adds auth token when a token is provided')
+test('adds auth token when a token is provided', () => { })
 // 🐨 create a fake token (it can be set to any string you want)
 // 🐨 create a "request" variable with let
 // 🐨 create a server handler to handle a test request you'll be making
@@ -39,14 +48,14 @@ test.todo('adds auth token when a token is provided')
 // 🐨 call the client with the token (note that it's async)
 // 🐨 verify that `request.headers.get('Authorization')` is correct (it should include the token)
 
-test.todo('allows for config overrides')
+test('allows for config overrides', () => { })
 // 🐨 do a very similar setup to the previous test
 // 🐨 create a custom config that specifies properties like "mode" of "cors" and a custom header
 // 🐨 call the client with the endpoint and the custom config
 // 🐨 verify the request had the correct properties
 
-test.todo(
-  'when data is provided, it is stringified and the method defaults to POST',
+test(
+  'when data is provided, it is stringified and the method defaults to POST', () => { }
 )
 // 🐨 create a mock data object
 // 🐨 create a server handler very similar to the previous ones to handle the post request
